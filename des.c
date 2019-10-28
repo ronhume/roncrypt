72ba50fd8ba561440ae1fb049887159372601bfc
Ron Hume <ronhume@ronhume.com>
2019-10-28 19:01:30 -0400

    add initial working des.c file

--- des.c
+++ des.c
@@ -1,177 +1,46 @@
+/*
+ *
+ * File: des.c
+ * Author: Ron Hume
+ * Copyright (c) 2019, All rights Reserved.
+ * 
+ */
+
 #include "common.h"
+#include "des_tables.h"
 
-#if 0
-/* ORIGINAL DES TABLE (big endian) */
-static const uint8_t spec_init_permutation[] = {
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
-    57, 49, 41, 33, 25, 17, 9, 1,
-    59, 51, 43, 35, 27, 19, 11, 3, 
-    61, 53, 45, 37, 29, 21, 13, 5, 
-    63, 55, 47, 39, 31, 23, 15, 7 };
-
-static const uint8_t spec_final_permutation[] = { 
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41,  9, 49, 17, 57, 25 };
-
-static const uint8_t spec_pc1_table_left[] = {
-    57, 49, 41, 33, 25, 17,  9, 1,
-    58, 50, 42, 34, 26, 18, 10, 2,
-    59, 51, 43, 35, 27, 19, 11, 3,
-    60, 52, 44, 36 };
-
-static const uint8_t spec_pc1_table_right[] = {
-    63, 55, 47, 39, 31, 23, 15, 7,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    61, 53, 45, 37, 29, 21, 13, 5,
-                    28, 20, 12, 4 };
-
-/* XXXX: recalculate PC2 table */
-static const uint8_t spec_pc2_table[] = { 
-    14, 17, 11, 24,  1,  5,
-     3, 28, 15,  6, 21, 10,
-    23, 19, 12,  4, 26,  8,
-    16,  7, 27, 20, 13,  2,
-    41, 52, 31, 37, 47, 55,
-    30, 40, 51, 45, 33, 48,
-    44, 49, 39, 56, 34, 53,
-    46, 42, 50, 36, 29, 32 };
-
-static void gen_little_endian_permutation_table(const uint8_t tbl[])
-{
-    uint8_t t[64];
-
-    for ( int i = 0; i < 64; i++ )
-    {
-        printf ( "%d, ", 64-tbl[i] );
-        if ( (i%8) == 7 )
-            printf("\n");
-        t[63-i] = 64-tbl[i];
-    }
-
-    printf("\n=================\n");
-
-    for ( int i = 0; i < 64; i++ )
-    {
-        printf("%d, ", t[i]);
-        if ( (i%8) == 7 )
-            printf("\n");
-    }
-    printf("\n");
-}
-
-static void gen_little_endian_pc1_table(const uint8_t tbl[])
-{
-    uint8_t t[28];
-
-    for ( int i = 0; i < 28; i++ )
-    {
-        printf ( "%d, ", 64-tbl[i] );
-        if ( (i%8) == 7 )
-            printf("\n");
-        t[27-i] = 64-tbl[i];
-    }
-
-    printf("\n=================\n");
-
-    for ( int i = 0; i < 28; i++ )
-    {
-        printf("%d, ", t[i]);
-        if ( (i%8) == 7 )
-            printf("\n");
-    }
-    printf("\n");
-}
-#endif
-
-/* RECALCUALTED TABLES (little-endian/zero indexed) */
-
-static const uint8_t initial_permutation_table[] = {
-    57, 49, 41, 33, 25, 17, 9, 1, 
-    59, 51, 43, 35, 27, 19, 11, 3, 
-    61, 53, 45, 37, 29, 21, 13, 5, 
-    63, 55, 47, 39, 31, 23, 15, 7, 
-    56, 48, 40, 32, 24, 16, 8, 0, 
-    58, 50, 42, 34, 26, 18, 10, 2, 
-    60, 52, 44, 36, 28, 20, 12, 4, 
-    62, 54, 46, 38, 30, 22, 14, 6 };
-
-static const uint8_t  final_permutation_table[] = {
-    39, 7, 47, 15, 55, 23, 63, 31, 
-    38, 6, 46, 14, 54, 22, 62, 30, 
-    37, 5, 45, 13, 53, 21, 61, 29, 
-    36, 4, 44, 12, 52, 20, 60, 28, 
-    35, 3, 43, 11, 51, 19, 59, 27, 
-    34, 2, 42, 10, 50, 18, 58, 26, 
-    33, 1, 41, 9, 49, 17, 57, 25, 
-    32, 0, 40, 8, 48, 16, 56, 24 };
-
-static const uint8_t pc1_table_left[] = {
-    28, 20, 12, 4, 61, 53, 45, 37, 
-    29, 21, 13, 5, 62, 54, 46, 38, 
-    30, 22, 14, 6, 63, 55, 47, 39, 
-    31, 23, 15, 7 };
-
-static const uint8_t pc1_table_right[] = {
-    60, 52, 44, 36, 59, 51, 43, 35, 
-    27, 19, 11,  3, 58, 50, 42, 34, 
-    26, 18, 10,  2, 57, 49, 41, 33, 
-    25, 17,  9,  1 };
-
-static const uint8_t pc2_table[] = { 
-#if 0
-    14, 17, 11, 24,  1,  5,
-     3, 28, 15,  6, 21, 10,
-    23, 19, 12,  4, 26,  8,
-    16,  7, 27, 20, 13,  2,
-    41, 52, 31, 37, 47, 55,
-    30, 40, 51, 45, 33, 48,
-    44, 49, 39, 56, 34, 53,
-    46, 42, 50, 36, 29, 32 };
-#else
-    24, 27, 20,  6, 14, 10,
-     3, 22,  0, 17,  7, 12,
-     8, 23, 11,  5, 16, 26,
-     1,  9, 19, 25,  4, 15,
-    54, 43, 36, 29, 49, 40, 
-    48, 30, 52, 44, 37, 33,
-    46, 35, 50, 41, 28, 53,
-    51, 55, 32, 45, 39, 42 };
-#endif
-
-static const uint8_t expansion_table[] = {
-    31,  0,  1,  2,  3,  4,
-     3,  4,  5,  6,  7,  8,
-     7,  8,  9, 10, 11, 12,
-    11, 12, 13, 14, 15, 16,
-    15, 16, 17, 18, 19, 20,
-    19, 20, 21, 22, 23, 24,
-    23, 24, 25, 26, 27, 28,
-    27, 28, 29, 30, 31,  0 };
-
-void permutation( uint64_t input, uint64_t *output, const uint8_t table[], size_t num )
+/* 
+ * Generic permutation function.
+ * Paramters: input, 
+ *            output (out param), 
+ *            table name, 
+ *            number of bits
+ *
+ * Return: void
+ */
+void permutation( uint64_t input, 
+                  uint64_t *output, 
+                  const uint8_t table[], 
+                  size_t num )
 {
     *output &= 0x0000000000000000;
 
     for (uint8_t i = 0; i < num; i++ )
-    {
         if (!!((1LL<<table[i]) & input))
-        {
-            //printf("set bit %d from bit %d\n", i, table[i]);
             *output |= (1LL<<i);
-        }
-    }
 }
 
-void rol_28_key(uint64_t *in_left, uint64_t *in_right, uint64_t *in_key)
+/* 
+ * Rotate 28-bit keys left, and combine output
+ * Paramters: in_left_key (out param), 
+ *            in_right_key (out param), 
+ *            out_key (out param)
+ *
+ * Return: void
+ */
+void rol_28_key(uint64_t *in_left, 
+                uint64_t *in_right, 
+                uint64_t *out_key)
 {
     *in_left <<= 1LL;
     if (!!(*in_left & 0x0000000010000000))
@@ -179,6 +48,7 @@ void rol_28_key(uint64_t *in_left, uint64_t *in_right, uint64_t *in_key)
       *in_left &= 0x000000000FFFFFFF;
       *in_left |= 1LL;
     }
+
     *in_right <<= 1LL;
     if (!!(*in_right & 0x0000000010000000))
     {
@@ -186,12 +56,43 @@ void rol_28_key(uint64_t *in_left, uint64_t *in_right, uint64_t *in_key)
       *in_right |= 1LL;
     }
 
-    *in_key = (*in_left << 28) | *in_right;
+    *out_key = (*in_left << 28) | *in_right;
+}
+
+/* 
+ * S-BOX operation
+ * Paramters: input
+ *            output (out param)
+ *
+ * Return: void
+ */
+#define NUM_SBOXES 8
+
+void do_sbox(uint64_t input, uint64_t *output)
+{
+    *output = 0LL;
+
+    uint8_t bits[NUM_SBOXES];
+
+    /* grab 6-bit valus */
+    for ( int i = 0; i < NUM_SBOXES; i++ )
+    {
+        bits[i] = input & 0x3FLL;
+        input >>= 6;
+    }
+
+    /* do SBOX substitutions */
+    for ( int i = 0; i < NUM_SBOXES; i++ )
+    {
+        *output |= sbox[i][bits[(NUM_SBOXES-1)-i]];
+        *output <<= 4;
+    }
+    *output >>=4;
 }
 
 void des_encrypt_block (uint64_t input, 
-                  uint64_t output,
-                  uint64_t iv,
+                  uint64_t *output,
+                  uint64_t cbc_iv,
                   uint64_t key )
 {
     uint64_t ip_block;
@@ -206,14 +107,14 @@ void des_encrypt_block (uint64_t input,
 
     unsigned int round;
 
-    printf ( "iv: 0x%" PRIx64 "\n", iv);
-    printf ( "input: 0x%" PRIx64 "\n", input);
-    input ^= iv;
-    printf ( "input_iv: 0x%" PRIx64 "\n", input);
-    permutation(input, &output, initial_permutation_table, 64);
-    printf ( "permuted: 0x%" PRIx64 "\n", output);
+    printf ( "IV: 0x%" PRIx64 "\n", cbc_iv);
+    printf ( "INPUT: 0x%" PRIx64 "\n", input);
+    input ^= cbc_iv;
+    printf ( "INPUT_IV: 0x%" PRIx64 "\n", input);
+    permutation(input, &ip_block, initial_permutation_table, 64);
+    printf ( "PERMUTED(IP): 0x%" PRIx64 "\n", ip_block);
 
-    printf ( "key: 0x%" PRIx64 "\n", key);
+    printf ( "KEY: 0x%" PRIx64 "\n", key);
     permutation(key, &pc1key_left, pc1_table_left, 28);
     //printf ( "PC1 LEFT : 0x%" PRIx64 "\n", pc1key_left);
     permutation(key, &pc1key_right, pc1_table_right, 28);
@@ -221,12 +122,12 @@ void des_encrypt_block (uint64_t input,
     pc1key = (pc1key_left << 28) | pc1key_right;
     printf ( "PC1 KEY: 0x%" PRIx64 "\n", pc1key);
     
-    uint64_t in_block = output&0x00000000FFFFFFFF;
-    printf ( "LOWER INPUT: 0x%" PRIx64 "\n", in_block);
     for ( int round = 0; round < 16; round++)
     {
         printf ("ROUND [%d]\n", round);
 
+        uint64_t in_block = ip_block&0x00000000FFFFFFFF;
+        //printf ( "LOWER INPUT: 0x%" PRIx64 "\n", in_block);
         permutation(in_block, &expansion_block, expansion_table, 48);
         printf ( "EXPANSION : 0x%" PRIx64 "\n", expansion_block);
         
@@ -244,15 +145,34 @@ void des_encrypt_block (uint64_t input,
         printf ( "SUBKEY : 0x%" PRIx64 "\n", subkey);
         expansion_block ^= subkey;
         printf ( "EXPANSION(XOR) : 0x%" PRIx64 "\n", expansion_block);
+
+        /* S-BOXes */
+        do_sbox(expansion_block,&substitution_block);
+        printf ( "SBOX: 0x%" PRIx64 "\n", substitution_block);
+
+        permutation(substitution_block, &pbox_target, permutation_table, 32);
+        printf ( "PBOX: 0x%" PRIx64 "\n", pbox_target);
+
+        /* swap sides */
+        uint64_t tmp_var = (ip_block>>32LL);
+        tmp_var ^= pbox_target;
+        ip_block = (ip_block<<32LL)|tmp_var;
+        
+        printf ( "OUTPUT: 0x%" PRIx64 "\n", ip_block);
     }
+
+    uint64_t tmp_var = (ip_block>>32LL);
+    ip_block = (ip_block<<32LL)|tmp_var;
+
+    permutation ( ip_block, output, final_permutation_table, 64 );
 }
 
 int main()
 {
     //unsigned char key[] = "password";
     uint64_t key = 0x70617373776F7264;
-    //unsigned char iv[] = "initialz";
-    uint64_t iv = 0x696E697469616C7A;
+    //unsigned char cbc_iv[] = "initialz";
+    uint64_t cbc_iv = 0x696E697469616C7A;
     //unsigned char input[] = "abcdefgh";
     uint64_t input = 0x6162636465666768 ;
 
@@ -269,7 +189,9 @@ int main()
 
     uint64_t output = 0LL;
 
-    des_encrypt_block( input, output, iv, key ); 
+    des_encrypt_block( input, &output, cbc_iv, key ); 
+    printf ( "DES: 0x%" PRIx64 "\n", output);
+
 }
 
 #if 0
