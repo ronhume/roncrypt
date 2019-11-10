3b2363ced7d5d78a59f25f8973854aabc82e94e1
Ron Hume <ronhume@ronhume.com>
2019-11-09 19:35:05 -0500

    start setting up AES directory

---
+++ aes/README.md
@@ -0,0 +1,2 @@
+# AES implemention
+
---
+++ aes/common.h
@@ -0,0 +1,23 @@
+/*
+ *
+ * File: common.h
+ * Author: Ron Hume
+ * Copyright (c) 2019, All rights Reserved.
+ * 
+ */
+
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#endif // __COMMON_H_
