5ace76042172b48f829a2fbf02077e316e90fb99
Ron Hume <ronhume@ronhume.com>
2019-11-16 14:56:52 -0500

    more granular tests - decrypt

---
+++ aes/tests/aes128_decrypt.c
@@ -0,0 +1,25 @@
+#include "common.h"
+#include "aes.h"
+
+#define IN_FILE "./enc.aes128"
+#define OUT_FILE "./dec.aes128"
+
+int main ()
+{
+    uint32_t salt[4] = { 0xFFFEFDFC,
+                         0xFBFAF9F8,
+                         0xF7F6F5F4,
+                         0xF3F2F1F0 };
+
+    uint32_t key[8] = { 0x01010101UL,
+                        0x01010101UL,
+                        0x01010101UL,
+                        0x01010101UL };
+
+    aes_file( IN_FILE,
+              OUT_FILE,
+              key,
+              salt,
+              KEY_128,
+              DECRYPT);
+}
