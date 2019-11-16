c07a8dee18000451e4555beb90a48fc4e291dd7e
Ron Hume <ronhume@ronhume.com>
2019-11-16 15:24:19 -0500

    script tests and capture results

--- aes/tests/testit.sh
+++ aes/tests/testit.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-echo "AES Encrypt"
-time ./aes_encrypt
-echo "AES Decrypt"
-time ./aes_decrypt
+echo "AES128 Encrypt"
+time ./aes128_encrypt
+echo "AES256 Encrypt"
+time ./aes256_encrypt
+echo "AES128 Decrypt"
+time ./aes128_decrypt
+echo "AES256 Decrypt"
+time ./aes256_decrypt
 
 echo "OpenSSL AES128 Decrypt"
 time openssl enc -d -aes-128-cbc -K 01010101010101010101010101010101 -iv fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0 -out dec_openssl.aes128 -in enc.aes128
@@ -27,8 +31,10 @@ echo "OpenSSL AES Encrypt"
 time openssl enc -aes-128-cbc -K 01010101010101010101010101010101 -iv fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0 -out enc.aes128 -in testfile.dat
 echo "OpenSSL AES Encrypt"
 time openssl enc -aes-256-cbc -K 0101010101010101010101010101010101010101010101010101010101010101 -iv fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0 -out enc.aes256 -in testfile.dat
-echo "AES Decrypt"
-time ./aes_decrypt
+echo "AES128 Decrypt"
+time ./aes128_decrypt
+echo "AES256 Decrypt"
+time ./aes256_decrypt
 
 md5 testfile.dat
 md5 dec.aes128
